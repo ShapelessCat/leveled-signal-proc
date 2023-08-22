342ef6fbebcb6ab5595a80dc1462ab6992d3fc40
Hao Hou <haohou302@gmail.com>
2023-08-22 16:10:38 +0800

    Add example for ealiest_event_to_state.py

---
+++ lsdl/examples/earliest_event_to_state.py
@@ -0,0 +1,16 @@
+from lsdl.schema import *
+from lsdl.modules import *
+from lsdl.signal_processors import *
+from lsdl import print_ir_to_stdout
+
+class Input(InputSchemaBase):
+    event = String()
+
+input = Input()
+
+# This state machine matches pattern "..*" and we check if the state is the state when we seen first state
+is_earliest_event = (StateMachine(clock = input.event.clock(), data = Const(1), transition_fn = "|&s, _| (s+1).min(2)") == 1)
+earliest_event_value = Latch(control = is_earliest_event, data = input.event, output_type = "&str")
+earliest_event_value.add_metric("earliestEventName")
+
+print_ir_to_stdout()
--- lsdl/lsdl/signal_processors/latch.py
+++ lsdl/lsdl/signal_processors/latch.py
@@ -2,7 +2,7 @@ from lsdl.componet_base import BuiltinComponentBase
 from lsdl.signal import LeveledSignalBase
 
 class Latch(BuiltinComponentBase):
-    def __init__(self, control: LeveledSignalBase, data: LeveledSignalBase, forget_duration = -1):
+    def __init__(self, control: LeveledSignalBase, data: LeveledSignalBase, forget_duration = -1, **kwargs):
         if forget_duration < 0:
             node_decl = "Latch::<{type_name}>::default()".format(type_name = data.get_rust_type_name())
         else:
@@ -16,3 +16,5 @@ class Latch(BuiltinComponentBase):
             node_decl = node_decl, 
             upstreams = [control, data]
         )
+        if "output_type" in kwargs:
+            self._output_type = kwargs["output_type"]
