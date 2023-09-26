b5e791cf33e6a0ab635ef62eeb244469c4551023
Hao Hou <haohou302@gmail.com>
2023-09-26 09:29:33 +0800

    Add missing file

---
+++ lsdl/lsdl/measurements/diff.py
@@ -0,0 +1,12 @@
+from lsdl.componet_base import BuiltinComponentBase
+from lsdl.signal import LeveledSignalBase
+
+class DiffSinceCurrentLevel(BuiltinComponentBase):
+    def __init__(self, control: LeveledSignalBase, data: LeveledSignalBase):
+        super().__init__(
+            name = "DiffSinceCurrentLevel",
+            is_measurement = True,
+            node_decl = "DiffSinceCurrentLevel::default()",
+            upstreams = [control, data],
+        )
+        self._output_type = data.get_rust_type_name()
