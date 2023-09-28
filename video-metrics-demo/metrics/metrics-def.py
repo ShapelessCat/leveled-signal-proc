93003c9a9bffb450f25b958367a92efcdae9f5ad
Hao Hou <haohou302@gmail.com>
2023-09-28 09:37:04 +0800

    x

--- video-metrics-demo/metrics/metrics-def.py
+++ video-metrics-demo/metrics/metrics-def.py
@@ -18,10 +18,19 @@ class Input(InputSchemaBase):
     ev             = named("ev",          String())
 
 input = Input()
+num_sid = input.session_id.count_changes()
+
+def sessionized(signal, signal_clock, default):
+    global input, num_sid
+    session_epoch = EdgeTriggeredLatch(control = num_sid, data = input.session_id.clock())
+    event_epoch = EdgeTriggeredLatch(control = signal_clock, data = input.session_id.clock())
+    return SignalMapper(
+        bind_var = "(sep, eep, signal)", 
+        lambda_src = f"if *sep <= *eep {{ *signal }} else {{ {default} }}", 
+        upstream = [session_epoch, event_epoch, signal])
 
 # Buffering time per session
 
-num_sid = input.session_id.count_changes()
 is_buffering = (input.player_state == PS_BUFFERING)
 is_buffering.measure_duration_true(scope_signal = num_sid).add_metric("bufferingTime")
 
@@ -34,15 +43,9 @@ num_ps = input.player_state.map(bind_var = "s", lambda_src = f"""
     }}
 """)
 
-ps_event_ts = EdgeTriggeredLatch(input.player_state.clock(), input.session_id.clock())
-session_ts = EdgeTriggeredLatch(num_sid, input.session_id.clock())
-sessionized_ps = SignalMapper(
-    bind_var = "(session_ts, ets, ps)", 
-    lambda_src = "if *ets < *session_ts { -1 } else {*ps}", 
-    upstream = [session_ts, ps_event_ts, num_ps])
-sessionized_ps.add_metric("playerState", "i32")
+player_state = sessionized(num_ps, input.player_state.clock(), "-1")
 
-has_been_playing = StateMachineBuilder(input.session_id.clock(), sessionized_ps)\
+has_been_playing = StateMachineBuilder(input.session_id.clock(), player_state)\
     .transition_fn("|&res: &bool, &state: &i32| res || state == 0")\
     .scoped(num_sid)\
     .build()
