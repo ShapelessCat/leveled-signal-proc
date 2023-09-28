b91b800a718b0025e1c9eb6aaee7f2827e79c689
Hao Hou <haohou302@gmail.com>
2023-09-28 15:44:13 +0800

    cleanup the video metrics example

--- examples/video-metrics/metrics/metrics-def.py
+++ examples/video-metrics/metrics/metrics-def.py
@@ -17,35 +17,30 @@ class Input(SessionizedInputSchemaBase):
     cdn            = named("CDN",         String())
     bit_rate       = named("BitRate",     Integer())
     ev             = named("ev",          String())
-    # Sessionized signal descriptions
+    
     bit_rate_default = -1
-
     def create_epoch_signal(self) -> LeveledSignalBase:
         return self.session_id.clock()
-
     def create_session_signal(self) -> LeveledSignalBase:
         return self.session_id.count_changes()
 
-
 input = Input()
 
-
-# State
+# Sessionized states
 input.sessionized_player_state.add_metric("playerState")
 input.sessionized_cdn.add_metric("cdn")
 input.sessionized_bit_rate.add_metric("bitrate")
 
-# Buffering time per session
-
+# Total buffering time per session
 is_buffering = (input.sessionized_player_state == PS_BUFFERING)
 is_buffering.measure_duration_true(scope_signal = input.session_signal).add_metric("bufferingTime")
 
+## Initial buffering time
 has_been_playing = StateMachineBuilder(input.session_id.clock(), input.player_state)\
     .transition_fn(f"|&res: &bool, state: &String| res || state == \"{PS_PLAYING}\"")\
     .scoped(input.session_signal)\
     .build()
 
-## Initial buffering time
 is_init_buffering = (~has_been_playing & is_buffering)
 is_init_buffering.measure_duration_true(scope_signal = input.session_signal).add_metric("initialBufferingTime")
 
@@ -56,8 +51,5 @@ is_re_buffering.measure_duration_true(scope_signal = input.session_signal).add_m
 # ev - seek time
 is_seek_start = (input.ev == EV_SEEK_START)
 is_seek_start.measure_duration_true(scope_signal = input.session_signal).add_metric("seekTime")
-
-# Debug
-input.session_id.peek().add_metric("sessionId")
 
 print_ir_to_stdout()
