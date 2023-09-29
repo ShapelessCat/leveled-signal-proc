cfd270dfa3a75226b7a1c089702b90655bcb67db
Hao Hou <haohou302@gmail.com>
2023-09-29 08:41:33 +0800

    fix the compilation error for earliest_event_to_state.py

--- lsdl/examples/earliest_event_to_state.py
+++ lsdl/examples/earliest_event_to_state.py
@@ -11,7 +11,7 @@ class Input(InputSchemaBase):
 input = Input()
 
 # This state machine matches pattern "..*" and we check if the state is the state when we seen first state
-is_earliest_event = (StateMachine(clock = input.event.clock(), data = Const(1), transition_fn = "|&s, _| (s+1).min(2)") == 1)
+is_earliest_event = (StateMachine(clock = input.event.clock(), data = Const(1), transition_fn = "|&s:&i32, _| (s+1).min(2)") == 1)
 earliest_event_value = Latch(control = is_earliest_event, data = input.event, output_type = "String")
 earliest_event_value.add_metric("earliestEventName")
 
