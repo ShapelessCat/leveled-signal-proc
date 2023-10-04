2e1a85e53dd3a61e5905b4bb736ae33f6c1d9a45
Hao Hou <haohou302@gmail.com>
2023-10-04 12:24:36 +0800

    prepare for cpu profiling

--- examples/video-metrics/src/main.rs
+++ examples/video-metrics/src/main.rs
@@ -1,21 +1,21 @@
-use std::{io::BufReader, fs::File};
+use std::{io::{BufReader, Write}, fs::File};
 
 use anyhow::Error;
 use lsp_runtime::instrument::NoInstrument;
 
 lsp_codegen::include_lsp_ir!(lsp_main @ "src/metrics-def.json");
 
-fn print_to_stdout(metrics: MetricsBag) -> Result<(), Error> {
-    println!("{}", serde_json::to_string(&metrics)?);
-    Ok(())
-}
-
 fn main() -> Result<(), Error> {
     let path = std::env::args().nth(1).expect("Missing path argument");
     let fp = BufReader::new(File::open(path)?);
     let input_stream = serde_json::Deserializer::from_reader(fp).into_iter().filter_map(|r| r.ok());
     let mut instr_ctx = NoInstrument::default();
-    lsp_main(input_stream, print_to_stdout, &mut instr_ctx)?;
+    let mut output = std::io::BufWriter::new(std::io::stdout());
+    lsp_main(input_stream, move |metric| {
+        output.write_all(serde_json::to_string(&metric)?.as_bytes())?;
+        output.write_all(b"\n")?;
+        Ok(())
+    }, &mut instr_ctx)?;
     eprintln!("{}", instr_ctx);
     Ok(())
 }
