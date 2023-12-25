3b450f25bebb74f9a14fcada4d36215eb56bfa9a
Lan, Jian <jilan@conviva.com>
2023-12-25 15:16:46 +0800

    Run autopep8 on demos/video-metrics

--- demos/video-metrics/metrics/metrics-def.py
+++ demos/video-metrics/metrics/metrics-def.py
@@ -12,27 +12,32 @@ input_signal.sessionized_bit_rate.add_metric("bitrate")
 # Total buffering time per session
 is_buffering = input_signal.sessionized_player_state == const.PS_BUFFERING
 is_buffering\
-    .measure_duration_true(scope_signal = input_signal.session_signal)\
+    .measure_duration_true(scope_signal=input_signal.session_signal)\
     .add_metric("bufferingTime")
 
 # - Initial buffering time
-has_been_playing = StateMachineBuilder(input_signal.session_id.clock(), input_signal.player_state)\
-    .transition_fn(f"|&res: &bool, state: &String| res || state == \"{const.PS_PLAYING}\"")\
-    .scoped(input_signal.session_signal)\
+has_been_playing = (
+    StateMachineBuilder(
+        input_signal.session_id.clock(),
+        input_signal.player_state
+    )
+    .transition_fn(f"|&res: &bool, state: &String| res || state == \"{const.PS_PLAYING}\"")
+    .scoped(input_signal.session_signal)
     .build()
+)
 
 (~has_been_playing & is_buffering)\
-    .measure_duration_true(scope_signal = input_signal.session_signal)\
+    .measure_duration_true(scope_signal=input_signal.session_signal)\
     .add_metric("initialBufferingTime")
 
 # - Re-buffering time
 (has_been_playing & is_buffering)\
-    .measure_duration_true(scope_signal = input_signal.session_signal)\
+    .measure_duration_true(scope_signal=input_signal.session_signal)\
     .add_metric("rebufferingTime")
 
 # ev - seek time
 (input_signal.ev == const.EV_SEEK_START)\
-    .measure_duration_true(scope_signal = input_signal.session_signal)\
+    .measure_duration_true(scope_signal=input_signal.session_signal)\
     .add_metric("seekTime")
 
 processing_config().set_merge_simultaneous_moments(should_merge=False)
