2512df802c97bfaac3e4f5ae52b6426daf902dfe
Lan, Jian <jilan@conviva.com>
2024-01-01 15:52:56 +0800

    Expose measurement combinators

--- lsp-component/src/measurements/combinator/mod.rs
+++ lsp-component/src/measurements/combinator/mod.rs
@@ -1,2 +1,5 @@
 pub mod mapper;
 pub mod scope;
+
+pub use scope::{ScopedMeasurement, ScopedMeasurementExt};
+pub use mapper::{MappedMeasurement, MappedMeasurementExt};
--- lsp-component/src/measurements/mod.rs
+++ lsp-component/src/measurements/mod.rs
@@ -2,7 +2,7 @@ pub mod combinator;
 mod duration;
 mod linear_change;
 mod peek;
-
+// TODO: Remove all ScopedXXXX type alias after adding direct measurement combinator support in LSDL and Codegen!
 pub use duration::{
     DurationSinceBecomeTrue, DurationSinceLastLevel, DurationTrue, ScopedDurationTrue,
 };
