c77a4822f2d67538f6562ce9114b8b53d22fa2b2
Lan, Jian <jilan@conviva.com>
2024-01-08 16:41:13 +0800

    Add a metric for testing the new added BinaryCombinedMeasurement

--- demos/experiment/metrics/metrics-def.py
+++ demos/experiment/metrics/metrics-def.py
@@ -1,4 +1,5 @@
 # extra-src: const.py schema.py scope.py first_video_attempt.py
+from lsdl.measurements.combinators.binary import BinaryCombinedMeasurement
 from lsdl.prelude import *
 from schema import input_signal
 
@@ -9,13 +10,14 @@ import first_video_attempt
 import scope
 
 input_signal.peek_timestamp().add_metric("ts")
-# input_signal.peek_timestamp("|x| x + 1").add_metric("ts_plus1", "u64")
 
-input_signal.peek_timestamp().map("x", "x + 1").add_metric("ts_plus1", "u64")
-input_signal.peek_timestamp().map("x", "x + 1").map("y", "y + 2").add_metric("ts_plus1_plus2", "u64")
+ts_plus1 = input_signal.peek_timestamp().map("x", "x + 1").add_metric("ts_plus1", "u64")
+ts_plus1_plus2 = input_signal.peek_timestamp().map("x", "x + 1").map("y", "y + 2").add_metric("ts_plus1_plus2", "u64")
+ts_plus1.combine('x', 'y', 'y - x', ts_plus1_plus2).add_metric("diff", 'u64')
 
-# input_signal.encoded_fps.measure_linear_change().add_metric("encoded_frames")
-# input_signal.inferred_rendered_fps.measure_linear_change().add_metric("inferred_rendered_frames")
+
+input_signal.encoded_fps.measure_linear_change().add_metric("encoded_frames")
+input_signal.inferred_rendered_fps.measure_linear_change().add_metric("inferred_rendered_frames")
 
 measurement_config().enable_measure_for_event()
 
