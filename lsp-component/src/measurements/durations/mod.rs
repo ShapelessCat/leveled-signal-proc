0e0c2cdb58811d8bdae1381e00667000e177bc69
Lan, Jian <jilan@conviva.com>
2024-03-24 13:25:57 +0800

    Split measurements duration module for improving maintainability

---
+++ lsp-component/src/measurements/durations/mod.rs
@@ -0,0 +1,7 @@
+mod duration_since_become_true;
+mod duration_since_last_level;
+mod duration_true;
+
+pub use duration_since_become_true::DurationSinceBecomeTrue;
+pub use duration_since_last_level::DurationSinceLastLevel;
+pub use duration_true::DurationTrue;
