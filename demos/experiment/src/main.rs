72d5daf71c67914a32032f2482963b96c0ecefeb
Lan, Jian <shapeless.cat@gmail.com>
2024-10-24 21:10:33 +0800

    --- remove

--- demos/experiment/src/main.rs
+++ demos/experiment/src/main.rs
@@ -7,27 +7,32 @@ use std::{
 use anyhow::Error;
 use lsp_runtime::instrument::NoInstrument;
 
-lsp_codegen::include_lsp_ir!(lsp_main @ "src/metrics-def.json");
+// lsp_codegen::include_lsp_ir!(lsp_main @ "src/metrics-def.json");
+// typify::import_types!("/home/shapeless-cat/Learning/leveled-signal-proc/demos/experiment/src/out-new.json");
+typify::import_types!(
+    schema = "/home/shapeless-cat/Learning/leveled-signal-proc/demos/experiment/src/all-new.json",
+    unknown_crates = Allow,
+);
 
 fn main() -> Result<(), Error> {
-    let path = std::env::args().nth(1).expect("Missing path argument");
-    let fp = BufReader::new(File::open(path)?);
-    let input_stream = serde_json::Deserializer::from_reader(fp)
-        .into_iter()
-        .filter_map(Result::ok);
-    let mut instr_ctx = NoInstrument;
-    let mut output = std::io::BufWriter::new(std::io::stdout());
-    let checkpoint_home = Path::new("./demos/experiment");
-    lsp_main(
-        input_stream,
-        move |metric| {
-            output.write_all(serde_json::to_string(&metric)?.as_bytes())?;
-            output.write_all(b"\n")?;
-            Ok(())
-        },
-        &mut instr_ctx,
-        checkpoint_home,
-    )?;
-    eprintln!("{}", instr_ctx);
+    // let path = std::env::args().nth(1).expect("Missing path argument");
+    // let fp = BufReader::new(File::open(path)?);
+    // let input_stream = serde_json::Deserializer::from_reader(fp)
+    //     .into_iter()
+    //     .filter_map(Result::ok);
+    // let mut instr_ctx = NoInstrument;
+    // let mut output = std::io::BufWriter::new(std::io::stdout());
+    // let checkpoint_home = Path::new("./demos/experiment");
+    // lsp_main(
+    //     input_stream,
+    //     move |metric| {
+    //         output.write_all(serde_json::to_string(&metric)?.as_bytes())?;
+    //         output.write_all(b"\n")?;
+    //         Ok(())
+    //     },
+    //     &mut instr_ctx,
+    //     checkpoint_home,
+    // )?;
+    // eprintln!("{}", instr_ctx);
     Ok(())
 }
